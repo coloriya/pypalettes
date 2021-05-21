f58a3e340ffa57a765c6dc1550112c82494e908d
Ankur Seth <iankurseth@gmail.com>
2021-05-21 22:52:49 +0530

    added ngon palette generator

---
+++ data/gen.py
@@ -0,0 +1,51 @@
+
+import colorsys
+import json
+
+def get_ngon_palettes(n):
+	palettes = []
+	max_base_hue = 360 / n
+	jump = 360 / n # jump between adjacent colors
+	step = 2 # a new palette every 4 degrees
+
+	for x in range(0, int(max_base_hue / step)):
+		palette = []
+		base_hue = x * step
+		for color_index in range(0, n):
+			hue = base_hue + (jump * color_index)
+			c1 = [round(hue/360, 2), 0.80, 0.45]
+			c2 = [round(hue/360, 2), 0.80, 0.55]
+			palette.append(c1)
+			palette.append(c2)
+		#print(palette)
+		palettes.append(palette)
+	return palettes
+
+
+palettes = []
+for x in range(2, 7):
+	palettes += get_ngon_palettes(x)
+
+pals = []
+index = 1
+for palette in palettes:
+	pal = {}
+	pal["id"] = index
+	pal["colors"] = []
+	for hsl_color in palette:
+		color = {}
+		color["hsl"] = hsl_color
+		rgb = colorsys.hls_to_rgb(*hsl_color)
+		rgb = [int(e * 256) for e in rgb]
+		color["rgb"] = rgb
+		color["hex"] = '#%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
+		pal["colors"].append(color)
+	pals.append(pal)
+	index += 1
+
+jo = {}
+jo["palettes"] = pals
+with open("palettes.json", "w") as f:
+	json.dump(jo, f)
+
+

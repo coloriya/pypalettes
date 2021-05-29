5286cf8509cee237f0f2f03f09269102ad3a1134
Ankur Seth <iankurseth@gmail.com>
2021-05-29 15:38:50 +0530

    now adding ids to palettes and checking for file existence

--- collect.py
+++ collect.py
@@ -10,14 +10,22 @@ def main():
 	palettes = []
 	for palette_json_name in palette_json_names:
 		palette_json_path = os.path.join("data", palette_json_name)
+		if not os.path.isfile(palette_json_path):
+			print(f"Not found: {palette_json_path}")
+			continue
 		pj = json.load(open(palette_json_path))
 		palettes += pj["palettes"]
+
+	index = 1
+	for palette in palettes:
+		palette["id"] = index
+		index += 1
 
 	jo = {}
 	jo["palettes"] = palettes
 	with open("data/palettes.json", "w") as f:
 		json.dump(jo, f)
-	print(f"{len(palettes)} palettes found.")
+	print(f"Collected {len(palettes)} palettes.")
 
 if __name__ == '__main__':
 	main()

0b531848d38331d0a571b854eed3337e39c3de58
Ankur Seth <iankurseth@gmail.com>
2021-05-29 15:04:20 +0530

    producing 180 analogous-triad palettes

---
+++ analogous_triad.py
@@ -0,0 +1,43 @@
+
+import json
+import math
+
+from pypalettes.color import PyColor
+
+saturations = [95, 75, 55, 35]
+lightnesses = [60, 40, 20]
+gaps = [15, 25, 35, 45, 55]
+
+palettes = []
+index = 1
+for h1 in range(0, 360, 10):
+	for gap in gaps:
+		h2 = (h1 + gap) % 360
+		h3 = (h2 + gap) % 360
+		hues = [h1, h2, h3]
+		print(hues)
+		palette = {}
+		palette["id"] = index
+		palette["type"] = "analogous-triad"
+		palette["hues"] = hues
+		palette["saturations"] = saturations
+		palette["lightnesses"] = lightnesses
+		palette["color_group_length"] = len(lightnesses) * len(saturations)
+		palette["colors"] = []
+
+		for hue in hues:
+			for saturation in saturations:
+				for lightness in lightnesses:
+					color = PyColor(hue, saturation, lightness)
+					palette["colors"].append(color.asObject())
+
+		palettes.append(palette)
+		index += 1
+		# break
+
+jo = {}
+jo["palettes"] = palettes
+with open("data/analogous_triad.json", "w") as f:
+	json.dump(jo, f)
+
+print(f"Produced {len(palettes)} analogous-triad palettes.")
